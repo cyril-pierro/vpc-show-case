59ef5197db1ae5fa472928b21c85317c71e334f5
cyril-pierro <fiopapa32@gmail.com>
2023-10-22 07:02:11 -0700

    add test script

---
+++ test/vpc_test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"testing"
+	"os"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+	"fmt"
+)
+
+func TestTerraformVPCProvisioning(t *testing.T) {
+	t.Parallel()
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../setup",
+	})
+
+	value:= fmt.Sprintf("deployed-on-%s", os.Getenv("TF_VAR_environment"))
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	output := terraform.Output(t, terraformOptions, "instance_tenancy")
+	assert.Equal(t, "default", output)
+
+	output = terraform.Output(t, terraformOptions, "tags_all")
+	fmt.Printf(output)
+	assert.Equal(t, "main-vpc-main", output)
+
+	output = terraform.Output(t, terraformOptions, "tags_all")
+	assert.Equal(t, value, output)
+
+}
