0143807ba7934023c65114876532f4b2f9a5a59f
cyril-pierro <fiopapa32@gmail.com>
2023-10-22 11:54:29 -0700

    add vpc log flow

--- setup/main.tf
+++ setup/main.tf
@@ -1,3 +1,10 @@
+resource "aws_flow_log" "example" {
+  iam_role_arn    = "arn"
+  log_destination = "log"
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.main.id
+}
+
 resource "aws_vpc" "main" {
   cidr_block           = var.cidr_block
   instance_tenancy     = "default"
